8b31c30d3c864eca6da8e53f611c8d5a4e5964bb
Suryavamshi Sirigadhi <58841159+surya1527@users.noreply.github.com>
2022-04-23 20:58:39 +0530

    Add files via upload

---
+++ Hash Table/Unique Morse Code Words.py
@@ -0,0 +1,18 @@
+from sortedcollections import OrderedSet
+
+
+words = ["gin","zen","gig","msg"]
+
+morse_code = [".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."]
+
+res=[]
+for i in words:
+    x=""
+    for j in i:
+        
+        index=ord(j)-97
+        x+= morse_code[index]
+    if x not in res:
+        res.append(x)
+
+print(len(res))
