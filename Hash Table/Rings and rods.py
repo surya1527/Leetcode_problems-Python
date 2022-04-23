5c34f581b32d00982a87a0af37ea197b30bfa8a3
Suryavamshi Sirigadhi <58841159+surya1527@users.noreply.github.com>
2022-04-23 21:00:25 +0530

    Add files via upload

---
+++ Hash Table/Rings and rods.py
@@ -0,0 +1,13 @@
+rings = "B0B6G0R6R0R6G9R0"
+d={}
+count = 0
+for i in range(1,len(rings),2):
+    if rings[i] not in d:
+        d[rings[i]]=[rings[i-1]]
+    else:
+        d[rings[i]].append(rings[i-1])       
+for i in d:
+    print(d)
+    if len(set(d[i]))==3:
+        count+=1
+print(count)
