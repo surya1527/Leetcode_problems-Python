f672e13007766c608555391d042ef3b1a93d8912
Suryavamshi Sirigadhi <58841159+surya1527@users.noreply.github.com>
2022-04-23 20:59:16 +0530

    Add files via upload

---
+++ Hash Table/smaller_than_current_number.py
@@ -0,0 +1,15 @@
+from pyparsing import nums
+
+
+nums = [8,1,2,2,3]
+
+ele = []
+for i in range(len(nums)):
+    count = 0
+    for j in range(len(nums)):
+        if nums[i] > nums[j]:
+            count+=1
+    ele.append(count)
+
+
+print(ele)
