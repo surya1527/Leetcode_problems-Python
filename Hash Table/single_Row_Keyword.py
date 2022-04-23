27d8bb21f5da16c89b0bb87c45e89c3542f591ac
Suryavamshi Sirigadhi <58841159+surya1527@users.noreply.github.com>
2022-04-23 20:59:51 +0530

    Add files via upload

---
+++ Hash Table/single_Row_Keyword.py
@@ -0,0 +1,16 @@
+from itertools import count
+
+
+keyboard = "abcdefghijklmnopqrstuvwxyz"
+word = "cba"
+sum=0
+initial=0
+for i in word:
+    for j in keyboard:
+        if i==j:
+            step= keyboard.index(j)
+            break
+    dif = abs(step-initial)            
+    sum += dif
+    initial=step
+print(sum)
