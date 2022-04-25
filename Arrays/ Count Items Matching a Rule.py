46b210f4dc7884fd137c657fe79ce6a605c7770c
Suryavamshi Sirigadhi <58841159+surya1527@users.noreply.github.com>
2022-04-25 19:44:39 +0530

    Add files via upload

---
+++ Arrays/ Count Items Matching a Rule.py
@@ -0,0 +1,15 @@
+from itertools import count
+
+
+items = [["phone","blue","pixel"],["computer","silver","lenovo"],["phone","gold","iphone"]]
+ruleKey = "color" 
+ruleValue = "silver"
+count = 0
+d  = {'type':0,'color':1,'name':2}
+for i in items:
+    if i[d[ruleKey]] == ruleValue:
+        count+=1
+
+
+print(count)
+        
