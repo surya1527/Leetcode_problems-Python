983cd19876bb74d7be44010a7294780bf1b60192
Suryavamshi Sirigadhi <58841159+surya1527@users.noreply.github.com>
2022-05-07 23:57:30 +0530

    Update Rings and rods.py

--- Hash Table/Rings and rods.py
+++ Hash Table/Rings and rods.py
@@ -3,5 +3,11 @@ d={}
 count = 0
 for i in range(1,len(rings),2):
     if rings[i] not in d:
-        d[rings
+        d[rings[i]]=[rings[i-1]]
+    else:
+        d[rings[i]].append(rings[i-1])        
+for i in d:
+    print(d)
+    if len(set(d[i]))==3:
+        count+=1
 print(count)
