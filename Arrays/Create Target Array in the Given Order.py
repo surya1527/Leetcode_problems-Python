35c5ab9979de66162d1955a17398a8f2706073ac
Suryavamshi Sirigadhi <58841159+surya1527@users.noreply.github.com>
2022-05-11 23:51:58 +0530

    Update Create Target Array in the Given Order.py

--- Arrays/Create Target Array in the Given Order.py
+++ Arrays/Create Target Array in the Given Order.py
@@ -2,11 +2,7 @@ nums = [0,1,2,3,4]
 index = [0,1,2,2,1]
 
 x=[]
-        
-        
-for i in range(0,len(nums)):
-    
-    x.insert(index[i], nums[i])
 
-    
+for i in range(0,len(nums)):
+    x.insert(index[i], nums[i])
 print(x) 
